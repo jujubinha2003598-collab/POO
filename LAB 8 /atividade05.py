bd1275e0465104ee6579eb822ff08be35d3c770b
Júlia Lara Machado Cirilo <jujubinha2003.598@gmail.com>
2026-06-15 10:45:10 -0300

    Add Onibus class with constructor

--- LAB 8 /atividade05.py
+++ LAB 8 /atividade05.py
@@ -1,1 +1,6 @@
-
+class Onibus:
+  def __init__ (self, placa: str, nome_motorista: str, num_assentos: int):
+    self.placa = placa
+    self.nome_motorista = nome
+    self.num_assentos = assentos
+    
